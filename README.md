2b8c8fb703fcf3b15b9e7b72ccb9680c9cb25084
qiuzhanghua <qiuzhanghua@icloud.com>
2024-03-03 18:38:57 +0800

    feat: simulate fatch from remote

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 pnpm build
 ```
 
+## Run
+```bash
+pnpm dev
+```
+
+## Observe
+- Open the browser console to see the output.
+- Click "Add Todo" Button to see the worker in action.
+- ```await window.obj.todos``` to get result.
+
 ## Reference
 - [Comlink Examples](https://github.com/GoogleChromeLabs/comlink/tree/main/docs/examples)
--- public/worker.js
+++ public/worker.js
@@ -1,13 +1,27 @@
-
-
 // importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 importScripts("/comlink/umd/comlink.js");
 
+async function fetchData(index) {
+	return fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
+			.then((response) => response.json())
+			.then((json) => {
+				return json;
+			})
+			.catch((error) => {
+				console.log(error)
+			});
+}
+
 const obj = {
-  counter: 0,
-  inc() {
-    this.counter++;
-  },
+	counter: 0,
+	todos: [],
+	inc() {
+		this.counter++;
+	},
+	async addTodo(index) {
+		const data = await fetchData(index);
+		this.todos.push(data);
+	}
 };
 
 Comlink.expose(obj);
